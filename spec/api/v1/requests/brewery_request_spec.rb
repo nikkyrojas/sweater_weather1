427131ed13616ca5aeaa6051e1ba26fbf157e4a7
Nikky Rojas <rojas7313@gmail.com>
2022-09-27 12:00:05 -0600

    addition test

--- spec/api/v1/requests/brewery_request_spec.rb
+++ spec/api/v1/requests/brewery_request_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Brewery Request" do 
   it 'finds a brewery by location' do
-
     get "/api/v1/breweries?location=denver&quantity=5"
 
     expect(response).to be_successful
